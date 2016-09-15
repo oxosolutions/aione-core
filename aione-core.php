f81d9068c52e863e6654e3e461a04ae721052abb
SGS Sandhu <contact@sgssandhu.com>
2016-09-16 00:18:42 +0530

    version 1.8.7.2

--- aione-core.php
+++ aione-core.php
@@ -3,7 +3,7 @@
 Plugin Name: Aione Core
 Plugin URI: http://oxosolutions.com
 Description: Aione Core Plugin for Aione Themes
-Version: 1.8.7.1
+Version: 1.8.7.2
 Author: OXO Solutions
 Author URI: http://oxosolutions.com
 GitHub Plugin URI: https://github.com/oxosolutions/aione-core
